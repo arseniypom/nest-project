e29a76621e2dba9a5dd899ffd034a77b686e9685
Arseniy Pomazkov <arpomazkov@bk.ru>
2022-10-12 17:46:57 +0300

    Added decorators for validation

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class AddRoleDto {
   @ApiProperty({ example: 'ADMIN', description: 'Role name' })
+  @IsString({ message: 'Value should be a string' })
   readonly value: string;
 
   @ApiProperty({ example: '63464cbbeb272d7feaaaecb6', description: 'User id' })
-  readonly userId: number;
+  @IsString({ message: 'UserId should be a string' })
+  readonly userId: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ example: 'abc@gmail.com', description: 'Email' })
+  @IsString({ message: 'Email should be a string' })
+  @IsEmail({}, { message: 'Invalid email' })
   readonly email: string;
 
   @ApiProperty({ example: 'qwerty12345', description: 'Password' })
+  @IsString({ message: 'Password should be a string' })
+  @Length(4, 50, {
+    message: 'Password should be at least 4 and maximum 50 characters',
+  })
   readonly password: string;
 }
